411df0633709b839ae622ca8f54254c0db0bc567
Elijah Sarver <elijah@thinq.com>
2020-03-03 13:32:39 -0500

    force receive-only channel pre-seed right before making the ticker make the ticker right before launching go func

--- zbt.go
+++ zbt.go
@@ -8,6 +8,7 @@ import (
 // Ticker ticks as soon as its created, then waits until duration to
 // tick again.
 type Ticker struct {
+	// C returns the time when a tick fires.
 	C <-chan time.Time
 
 	it         *time.Ticker
@@ -15,21 +16,27 @@ type Ticker struct {
 	cancelFunc context.CancelFunc
 }
 
-// NewTicker creates a Ticker that immediately fires into its channel.
+// NewTicker creates a Ticker that comes pre-loaded with the first tick.
 // duration must be non-zero or the underlying library will panic.
 func NewTicker(duration time.Duration) *Ticker {
-	ctx, done := context.WithCancel(context.Background())
+	// making c here so we can map it to a receive-only channel
+	// in the Ticker
 	c := make(chan time.Time, 1)
 	zbt := Ticker{
-		C:          c,
-		ctx:        ctx,
-		cancelFunc: done,
+		C: c,
 	}
+
+	zbt.ctx, zbt.cancelFunc = context.WithCancel(context.Background())
+
+	// pre-seed c with an immediate value
 	c <- time.Now()
+	// create this right before the goroutine in case duration is
+	// really small:
 	zbt.it = time.NewTicker(duration)
 
 	go func() {
-		// We have to select against both done and
+		// We have to select against both done and the channel
+		// in order to exit the goroutine cleanly on Close()
 		for {
 			select {
 			case <-zbt.ctx.Done():
@@ -43,11 +50,13 @@ func NewTicker(duration time.Duration) *Ticker {
 	return &zbt
 }
 
-// Stop passes the stop command onto the internal ticker.
+// Stop passes the stop command onto the internal ticker and
+// closes the internal context.Context so the goroutine doesn't
+// run forever.
 func (zbt *Ticker) Stop() {
 	zbt.it.Stop()
 	zbt.cancelFunc()
-	// drain
+	// drain the ticker to prevent accidental ticks.
 	select {
 	case _, _ = <-zbt.C:
 	}
